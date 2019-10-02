0a28a99e1d26b491e69aff35029e4c36ff1ee2bd
Arian Derida <arianderida@gmail.com>
2019-10-02 19:01:32 +0700

    Add files via upload

---
+++ src/Lib/DaftarMakanan.js
@@ -0,0 +1,29 @@
+const DaftarMakanan= [
+  {
+    gambar: "https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/nasipadang.jpg",
+    nama: "Nasi Padang",
+    harga: 20.000
+  },
+  {
+    gambar: "https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sate.png",
+    nama: "Sate",
+    harga: 20.000
+  },
+  {
+    gambar:"https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sotolamongan.png",
+    nama: "Soto",
+    harga: 10.000
+  },
+  {
+    gambar:"https://www.dbs.com/iwov-resources/images/newsroom/indonesia/Blog/masakan%20nusantara/nasi%20kentut.png",
+    nama: "Nasi Uduk",
+    harga: 10.000
+  },
+  {
+    gambar:"https://pesona.travel/media/nasi-kuning-sarapan-khas-nusantara-bermakna-mendalam_144651_1140.jpg",
+    nama: "Nasi Kuning",
+    harga: 15.000
+  }
+]
+
+export default DaftarMakanan;
---
+++ src/Lib/DaftarMinuman.js
@@ -0,0 +1,29 @@
+const DaftarMakanan= [
+  {
+    gambar: "https://www.gotravelly.com/blog/wp-content/uploads/2017/12/minuman-tradisional-indonesia-cendol.jpg",
+    nama: "Es Cendol",
+    harga: 10000
+  },
+  {
+    gambar: "https://www.gotravelly.com/blog/wp-content/uploads/2017/12/minuman-tradisional-indonesia-es-pisang-hijau.jpg",
+    nama: "Es Pisang Hijau",
+    harga: 10000
+  },
+  {
+    gambar:"https://www.gotravelly.com/blog/wp-content/uploads/2017/12/minuman-tradisional-indonesia-es-kacang-merah.jpg",
+    nama: "Es Kacang Merah",
+    harga: 10000
+  },
+  {
+    gambar:"https://www.gotravelly.com/blog/wp-content/uploads/2017/12/minuman-tradisional-indonesia-es-pleret.jpg",
+    nama: "Es Pleret",
+    harga: 10000
+  },
+  {
+    gambar:"https://www.gotravelly.com/blog/wp-content/uploads/2017/12/minuman-tradisional-indonesia-es-legen.jpg",
+    nama: "Es Legen",
+    harga: 15000
+  }
+]
+
+export default DaftarMakanan;
